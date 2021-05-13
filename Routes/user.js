6a3a7c2448db398dfb5b229137183284c1ba1069
Alexandrebelin <belin.alexandre.77@gmail.com>
2021-05-13 15:10:23 +0200

    updtae user

--- Routes/user.js
+++ Routes/user.js
@@ -15,52 +15,41 @@ const Offer = require("../Models/Offer");
 router.post("/user/signup", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.fields.email });
-    console.log(req);
-    let securePassword = req.fields.password;
-    let strongPassword =
-      /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*/]{8,}$/.test(
-        securePassword
-      );
 
     if (!user) {
       if (req.fields.email && req.fields.username && req.fields.password) {
-        if (strongPassword) {
-          const password = req.fields.password;
+        const password = req.fields.password;
+        const salt = uid2(64);
+        const hash = SHA256(password + salt).toString(encBase64);
+        const token = uid2(64);
 
-          const salt = uid2(64);
-          const hash = SHA256(password + salt).toString(encBase64);
-          const token = uid2(64);
+        const newUser = new User({
+          email: req.fields.email,
+          account: {
+            username: req.fields.username,
+            phone: req.fields.phone,
+          },
+          token: token,
+          hash: hash,
+          salt: salt,
+        });
 
-          const newUser = new User({
-            email: req.fields.email,
-            account: {
-              username: req.fields.username,
-              phone: req.fields.phone,
-            },
-            token: token,
-            hash: hash,
-            salt: salt,
+        if (req.files.avatar) {
+          let pictureToUpload = req.files.avatar.path;
+          const result = await cloudinary.uploader.upload(pictureToUpload, {
+            folder: `/Vinted/users/${newUser._id}`,
+            allowed_formats: ["png, jpg"],
           });
-
-          if (req.files.avatar) {
-            let pictureToUpload = req.files.avatar.path;
-            const result = await cloudinary.uploader.upload(pictureToUpload, {
-              folder: `/Vinted/users/${newUser._id}`,
-              allowed_formats: ["png, jpg"],
-            });
-            newUser.account.avatar = result;
-          }
-
-          const returnUser = (({ email, account, token, hash, salt, _id }) => ({
-            account,
-            token,
-            _id,
-          }))(newUser);
-          await newUser.save();
-          res.status(200).json(returnUser);
-        } else {
-          res.status(404).json({ error: "Password not strong enought" });
+          newUser.account.avatar = result;
         }
+
+        const returnUser = (({ email, account, token, hash, salt, _id }) => ({
+          account,
+          token,
+          _id,
+        }))(newUser);
+        await newUser.save();
+        res.status(200).json(returnUser);
       } else {
         res.status(404).json({ error: "Missing parameters" });
       }
